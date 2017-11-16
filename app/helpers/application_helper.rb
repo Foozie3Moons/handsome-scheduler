706151b8221ec0f005bd1e730e85179361d6331b
foozie3moons <matthew.bell.290@gmail.com>
2017-11-16 15:39:17 -0800

    added landing page, updated navbar styling, added google signin button (more official)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def is_active? link_path
     request.fullpath == link_path ? "is-active" : ""
   end
+
+  def landing_page?
+    request.fullpath == root_path
+  end
 end
