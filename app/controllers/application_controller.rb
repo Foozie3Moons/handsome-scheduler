1f686e7162063c3ef0ba15922851d0f445e98466
foozie3moons <matthew.bell.290@gmail.com>
2017-11-30 12:16:12 -0800

    proper logout after token expiration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,11 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
+    if @current_user
+      if @current_user.oauth_expires_at < Time.new
+        session[:user_id] = nil
+      end
+    end
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
